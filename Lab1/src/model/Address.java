38e4961020d082521f3c01aa31405da9b9c50f73
shriran <shrirangesh962@gmail.com>
2025-09-24 22:58:02 -0400

    final

--- Lab1/src/model/Address.java
+++ Lab1/src/model/Address.java
@@ -8,6 +8,51 @@ package model;
  *
  * @author vedanarayananshrirangesh
  */
-class Address {
+public class Address {
+    
+    String streetName;
+    String unitNum;
+    String city;
+    String zipCode;
+
+    public String getStreetName() {
+        return streetName;
+    }
+
+    public void setStreetName(String streetName) {
+        this.streetName = streetName;
+    }
+
+    public String getUnitNum() {
+        return unitNum;
+    }
+
+    public void setUnitNum(String unitNum) {
+        this.unitNum = unitNum;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public void setStreetName() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+   
+    
+
     
 }
